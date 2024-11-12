edbdb9e4029c3fe4bdde28973e7a092c37893562
Pedro Flores <pedrolucaspflores@gmail.com>
2024-11-12 03:05:55 -0300

    2nd commit

--- src/components/Selector/SelectorCard.tsx
+++ src/components/Selector/SelectorCard.tsx
@@ -1,3 +1,4 @@
+// SelectorCard.jsx
 'use client';
 import React, { useEffect, useState } from 'react';
 import styles from './Selector.module.css';
@@ -5,16 +6,28 @@ import Image from 'next/image';
 
 interface SelectorCardProps {
   selected: string;
+  isBlurred: boolean;
+  onHover: () => void;
+  onLeave: () => void;
+  myIndex: number; // Adicionado: Índice do card
 }
 
 interface CollectionData {
   name: string;
   floor: string;
   mainImage: string;
+  verified: boolean;
 }
 
-const SelectorCard = ({ selected }: SelectorCardProps) => {
+const SelectorCard: React.FC<SelectorCardProps> = ({
+  selected,
+  isBlurred,
+  onHover,
+  onLeave,
+  myIndex, // Recebe o índice como prop
+}: SelectorCardProps) => {
   const [data, setData] = useState<CollectionData | null>(null);
+  const [isMounted, setIsMounted] = useState(false); // Estado para controlar a montagem
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,25 +42,67 @@ const SelectorCard = ({ selected }: SelectorCardProps) => {
     fetchData();
   }, [selected]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     console.log(`${selected}:`, `/Collections/${data?.mainImage}.webp`);
   }, [data, selected]);
+
+  // Adiciona a classe 'animeLeft' após a montagem
+  useEffect(() => {
+    const timer = setTimeout(
+      () => {
+        setIsMounted(true);
+      },
+      myIndex * 50 > 500 ? myIndex * 100 : 0,
+    ); // Ajuste o atraso aqui (em milissegundos)
+
+    return () => clearTimeout(timer);
+  }, [myIndex]);
 
   if (!data) {
     return <p>Carregando dados...</p>;
   }
 
   return (
-    <div className={styles.card}>
-      <Image
-        src={`/Collections/${data.mainImage}.webp`} ///Orion/logo.png
-        alt="Collection Image"
-        width={1024}
-        height={1024}
-        className={styles.cardImage}
-      ></Image>
-      <h2>{selected}</h2>
-      <p>{data.name}</p>
+    <div
+      className={`${styles.cardWrapper} ${isBlurred ? styles.blurred : ''} ${
+        isMounted ? 'animeLeft' : ''
+      }`}
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+      style={{ animationDelay: `${myIndex * 0.2}s` }} // Define o delay baseado no índice
+      tabIndex={0} // Para acessibilidade via teclado
+    >
+      {/* Overlay para Hover */}
+      <div className={styles.cardOverlayHover}></div>
+
+      {/* Vários divs com a classe mousePositionTracker */}
+      {Array.from({ length: 9 }).map((_, index) => (
+        <div key={index} className={styles.mousePositionTracker}></div>
+      ))}
+
+      <div className={styles.cardContent}>
+        <Image
+          src={`/Collections/${data.mainImage}.webp`} // Orion/logo.png
+          alt="Collection Image"
+          width={1024}
+          height={1024}
+          className={styles.cardImage}
+        />
+        <div className={styles.cardOverlay}>
+          <h4>
+            {data.name}
+            {data.verified && (
+              <Image
+                src="/SVG/verified2.svg"
+                alt="Verified"
+                width={24}
+                height={24}
+              />
+            )}
+          </h4>
+          <p>{data.floor}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Selector/SelectorWrapper.tsx
+++ src/components/Selector/SelectorWrapper.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+// SelectorWrapper.jsx
+import React, { useState } from 'react';
 import styles from './Selector.module.css';
 import SelectorCard from './SelectorCard';
+
 const SelectorWrapper = ({ currentItems }) => {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
   return (
     <div className={styles.selectorWrapper}>
       {currentItems.map((item, index) => (
-        <SelectorCard key={index} selected={item} />
+        <SelectorCard
+          key={index}
+          selected={item}
+          isBlurred={hoveredIndex !== null && hoveredIndex !== index}
+          onHover={() => setHoveredIndex(index)}
+          onLeave={() => setHoveredIndex(null)}
+          myIndex={index}
+        />
       ))}
     </div>
   );
